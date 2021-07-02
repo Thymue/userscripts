57ea712e990ac2373bd1dfaa29b0120714fe2f82
Timotej Poľanský <thymuetv@gmail.com>
2021-07-02 15:58:02 +0200

    change some variable names

--- sscombined.user.js
+++ sscombined.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         osu!combined score-ranks
 // @namespace    http://thymo.ga/
-// @version      2020.1215.0
+// @version      2021.702.0
 // @description  Adds up s's and ss's
 // @author       Thymue
 // @include      https://osu.ppy.sh/*
@@ -13,7 +13,7 @@
 (() => {
   sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
   
-  doShitWithShit = async () => {
+  addIcons = async () => {
     if(document.location.href.startsWith("https://osu.ppy.sh/users/")) {
       if($(".profile-rank-count")[0] == undefined) await sleep(1000);
       ssh = parseInt($(".profile-rank-count")[1].children[0].innerText.replaceAll(",", ""));
@@ -21,24 +21,24 @@
       sh = parseInt($(".profile-rank-count")[1].children[2].innerText.replaceAll(",", ""));
       s = parseInt($(".profile-rank-count")[1].children[3].innerText.replaceAll(",", ""));
       
-      if($("#sshit-sscombined")[0] != undefined) $("#sshit-sscombined")[0].remove();
-      if($("#sshit-scombined")[0] != undefined) $("#sshit-scombined")[0].remove();
+      if($("#scorerank-sscombined")[0] != undefined) $("#scorerank-sscombined")[0].remove();
+      if($("#scorerank-scombined")[0] != undefined) $("#scorerank-scombined")[0].remove();
       
-      sscombined = $(`<div class="profile-rank-count__item" id="sshit-sscombined"><div class="score-rank score-rank--X score-rank--profile-page" style="filter: grayscale(1);"></div>${(ss + ssh).toLocaleString()}</div>`);
-      scombined = $(`<div class="profile-rank-count__item" id="sshit-scombined"><div class="score-rank score-rank--S score-rank--profile-page" style="filter: grayscale(1);"></div>${(s + sh).toLocaleString()}</div>`);
-      //scombined = $(`<div class="profile-rank-count__item" id="sshit-scombined"><div class="score-rank score-rank--S score-rank--profile-page"></div>${(s + sh).toLocaleString()}</div>`);
+      sscombined = $(`<div class="profile-rank-count__item" id="scorerank-sscombined"><div class="score-rank score-rank--X score-rank--profile-page" style="filter: grayscale(1);"></div>${(ss + ssh).toLocaleString()}</div>`);
+      scombined = $(`<div class="profile-rank-count__item" id="scorerank-scombined"><div class="score-rank score-rank--S score-rank--profile-page" style="filter: grayscale(1);"></div>${(s + sh).toLocaleString()}</div>`);
+      //scombined = $(`<div class="profile-rank-count__item" id="scorerank-scombined"><div class="score-rank score-rank--S score-rank--profile-page"></div>${(s + sh).toLocaleString()}</div>`);
       
       $(".profile-rank-count")[1].appendChild(sscombined[0]);
       $(".profile-rank-count")[1].appendChild(scombined[0]);
     }
   }
   
-  shitObserveShit = (e) => {
-    shitObserver.disconnect();
-    shitObserver.observe(document.body, {childList: true});
-    doShitWithShit();
+  observeMutations = (e) => {
+    mutationObserver.disconnect();
+    mutationObserver.observe(document.body, {childList: true});
+    addIcons();
   }
   
-  shitObserver = new MutationObserver(shitObserveShit)
-  shitObserver.observe(document.body, {childList: true});
+  const mutationObserver = new MutationObserver(observeMutations)
+  mutationObserver.observe(document.body, {childList: true});
 })();
